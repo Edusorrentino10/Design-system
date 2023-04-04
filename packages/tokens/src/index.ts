9553d00ae838b90500aefd637e8309004cdeadac
Eduardo <eduardo.sorrentino5@gmail.com>
2023-04-04 10:19:34 -0300

    add repo

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,14 @@
+export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+
+// import { colors } from '@ignite-ui/tokens/colors'
+
+//import { colors } from '@ignite-ui/tokens'
+// essa importacao no index faz nao precisar por o /colors no final.
